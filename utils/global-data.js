bcc73efcb74662d8f40273de1ff61332a75040c9
Muhamad Ridho Ardian <49930863+pockypaw@users.noreply.github.com>
2022-03-31 19:01:01 +0700

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Lorpha Blogs';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'MY DATING BLOGS';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Ardy & Dillah make in loves.';
 
   return {
     name,
